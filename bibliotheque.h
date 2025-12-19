f1d9d9f4bf543058838d40c4e369002f14795e67
Helderfox49 <mbottoguyzo2012@gmail.com>
2025-12-19 08:17:35 +0100

    Nettoyage du fichier bibliotheque.h

--- bibliotheque.h
+++ bibliotheque.h
@@ -1,36 +1,25 @@
 #ifndef __MY_MATH_LIB_H
 #define __MY_MATH_LIB_H
 
-
-// Définition de la fonction de calcul du factoriel.
 unsigned int fact( unsigned int value );
 
-// Défintion de la fonction de calcul de la puissance.
 int power(int value, unsigned int pow);
 
-//Définition d'une fonction qui résoud une équation du 2nd dégré
 void resolve_equation(double a, double b, double c);
 
-// Définition d'une fonction qui détermine si un nombre entier est premier
 int is_prime(int n);
 
-//Définition de la structure Point
 typedef struct {
     double x;
     double y;
 } Point;
 
-//Défintion d'une fonction qui crèe un nouveau point
 Point create_point(double x, double y);
 
-//Définition d'une fonction qui calcule la distance entre 02 points
 double distance(const Point *A, const Point *B);
 
-//Définition d'une fonction qui permute la valeur de 02 nombres entiers
 void swap(int *a, int *b);
 
-// Définition d'une fonction  utilitaire pour vider le buffer
 void empty_buffer(void);
-
 
 #endif
